685a05ceb5ed45ca5fcdb5483adf15e237e9c15f
Henry Dang <henrydang@fossil.com>
2023-06-07 20:27:56 +0700

    Fixed build script, gcc, gdb work well

--- build_arm_toolchains_win_mingw64.sh
+++ build_arm_toolchains_win_mingw64.sh
@@ -9,9 +9,10 @@ export PATCH_DIR="${WORK_DIR}/patch"
 export DOWNLOAD_DIR="${WORK_DIR}/download"
 export INSTALL_DIR="${WORK_DIR}/install"
 export BUILD_DIR="${WORK_DIR}/build"
-export PREFIX_WIN="${INSTALL_DIR}/custom-arm-none-eabi"
+export SOURCE_DIR="${WORK_DIR}/source"
+export PREFIX_TARGET="${INSTALL_DIR}/custom-arm-none-eabi"
 export PREFIX_LINUX="${INSTALL_DIR}/x86_64-pc-linux-gnu"
-export PREFIX_TARGET="${PREFIX_WIN}"
+#export SYSROOT="${INSTALL_DIR}/x86_64-w64-mingw32"
 
 # export PATH for the boostrap Linux arm-none-eabi-gcc
 export PATH=$PATH:"${PREFIX_LINUX}/bin"
@@ -21,6 +22,10 @@ read -n 1 -p "Press return to continue"
 
 if [ ! -e "$DOWNLOAD_DIR" ]; then
     mkdir -p $DOWNLOAD_DIR
+fi
+
+if [ ! -e "$SOURCE_DIR" ]; then
+    mkdir -p $SOURCE_DIR
 fi
 
 if [ ! -e "$BUILD_DIR" ]; then
@@ -92,20 +97,20 @@ declare -A source_list=(
 echo "Extracting source files..."
 while [[ ${#source_list[@]} -gt 0 ]]; do
     for source in "${!source_list[@]}"; do
-        if [ -d "${BUILD_DIR}/${source}" ]; then
+        if [ -d "${SOURCE_DIR}/${source}" ]; then
             # echo "Source '$source' exists"
             unset 'source_list[$source]'  # Remove the source file from the list
         else
             # echo "'$source' not exit. Extracting..."
             tarball="${source_list[$source]}"
-            tar -xf "${DOWNLOAD_DIR}/${tarball}" -C "${BUILD_DIR}"
+            tar -xf "${DOWNLOAD_DIR}/${tarball}" -C "${SOURCE_DIR}"
             if [[ $? -eq 0 ]]; then
                 # echo "File '$source' extract successfully"
-                if [ ! -e "${BUILD_DIR}/${source}-build-windows" ]; then
-                    mkdir -p "${BUILD_DIR}/${source}-build-windows"
+                if [ ! -e "${BUILD_DIR}/windows/${source}-build" ]; then
+                    mkdir -p "${BUILD_DIR}/windows/${source}-build"
                 fi
-                if [ ! -e "${BUILD_DIR}/${source}-build-linux" ]; then
-                    mkdir -p "${BUILD_DIR}/${source}-build-linux"
+                if [ ! -e "${BUILD_DIR}/linux/${source}-build" ]; then
+                    mkdir -p "${BUILD_DIR}/linux/${source}-build"
                 fi
                 unset 'source_list[$source]'  # Remove the file from the list
             else
@@ -119,76 +124,76 @@ done
 
 ####################### Apply patching source code ##########################
 
-if [ ! -f "${BUILD_DIR}/libiconv-1.17/libiconv-1.17.patch" ]; then
-    cd "${BUILD_DIR}/libiconv-1.17"
+if [ ! -f "${SOURCE_DIR}/libiconv-1.17/libiconv-1.17.patch" ]; then
+    cd "${SOURCE_DIR}/libiconv-1.17"
     patch -p1 < "${PATCH_DIR}/libiconv-1.17.patch"
-    cp "${PATCH_DIR}/libiconv-1.17.patch" "${BUILD_DIR}/libiconv-1.17"
+    cp "${PATCH_DIR}/libiconv-1.17.patch" "${SOURCE_DIR}/libiconv-1.17"
 fi
 
 ################# Build Linux bootstrap arm-none-eabi-gcc ###################
 
-cd "${BUILD_DIR}/gmp-6.1.0-build-linux"
+cd "${BUILD_DIR}/linux/gmp-6.1.0-build"
 if [ ! -f "Makefile" ]; then
-    ../gmp-6.1.0/configure     \
-    --prefix="${PREFIX_LINUX}" \
-    --enable-fft               \
-    --enable-cxx               \
-    --disable-shared           \
-    --enable-static            \
-    CPPFLAGS='-I/usr/include'  \
+    ${SOURCE_DIR}/gmp-6.1.0/configure \
+    --prefix="${PREFIX_LINUX}"        \
+    --enable-fft                      \
+    --enable-cxx                      \
+    --disable-shared                  \
+    --enable-static                   \
+    CPPFLAGS='-I/usr/include'         \
     LDFLAGS='-L/usr/lib/x86_64-linux-gnu'
 fi
 make -j`nproc` && make install
 
-cd "${BUILD_DIR}/mpfr-4.2.0-build-linux"
+cd "${BUILD_DIR}/linux/mpfr-4.2.0-build"
 if [ ! -f "Makefile" ]; then
-    ../mpfr-4.2.0/configure      \
-    --prefix="${PREFIX_LINUX}"   \
-    --with-gmp="${PREFIX_LINUX}" \
+    ${SOURCE_DIR}/mpfr-4.2.0/configure \
+    --prefix="${PREFIX_LINUX}"         \
+    --with-gmp="${PREFIX_LINUX}"       \
     --disable-shared --enable-static
 fi
 make -j`nproc` && make install
 
-cd "${BUILD_DIR}/mpc-1.3.1-build-linux"
+cd "${BUILD_DIR}/linux/mpc-1.3.1-build"
 if [ ! -f "Makefile" ]; then
-    ../mpc-1.3.1/configure        \
-    --prefix="${PREFIX_LINUX}"    \
-    --with-gmp="${PREFIX_LINUX}"  \
-    --with-mpfr="${PREFIX_LINUX}" \
+    ${SOURCE_DIR}/mpc-1.3.1/configure \
+    --prefix="${PREFIX_LINUX}"        \
+    --with-gmp="${PREFIX_LINUX}"      \
+    --with-mpfr="${PREFIX_LINUX}"     \
     --disable-shared --enable-static
 fi
 make -j`nproc` && make install
 
 # binutils-2.40
-cd "${BUILD_DIR}/binutils-2.40-build-linux"
+cd "${BUILD_DIR}/linux/binutils-2.40-build"
 if [ ! -f "Makefile" ]; then
-    ../binutils-2.40/configure    \
-    --prefix="${PREFIX_LINUX}"    \
-    --target="${TARGET}"          \
-    --with-gmp="${PREFIX_LINUX}"  \
-    --with-mpfr="${PREFIX_LINUX}" \
+    ${SOURCE_DIR}/binutils-2.40/configure \
+    --prefix="${PREFIX_LINUX}"            \
+    --target="${TARGET}"                  \
+    --with-gmp="${PREFIX_LINUX}"          \
+    --with-mpfr="${PREFIX_LINUX}"         \
     --with-mpc="${PREFIX_LINUX}"
 fi
 make -j`nproc` && make install
 
 # gcc-13.1.0
-cd "${BUILD_DIR}/gcc-13.1.0-build-linux"
+cd "${BUILD_DIR}/linux/gcc-13.1.0-build"
 if [ ! -f "Makefile" ]; then
-    ../gcc-13.1.0/configure       \
-    --prefix="${PREFIX_LINUX}"    \
-    --target="${TARGET}"          \
-    --with-gmp="${PREFIX_LINUX}"  \
-    --with-mpfr="${PREFIX_LINUX}" \
-    --with-mpc="${PREFIX_LINUX}"  \
-    --disable-multilib            \
-    --disable-shared              \
-    --disable-nls                 \
-    --enable-languages=c,c++      \
-    --with-cpu=cortex-a7          \
-    --with-fpu=neon-vfpv4         \
-    --with-float=hard             \
-    --with-newlib                 \
-    --with-headers="${BUILD_DIR}/newlib-4.3.0.20230120/newlib/libc/include"
+    ${SOURCE_DIR}/gcc-13.1.0/configure \
+    --prefix="${PREFIX_LINUX}"         \
+    --target="${TARGET}"               \
+    --with-gmp="${PREFIX_LINUX}"       \
+    --with-mpfr="${PREFIX_LINUX}"      \
+    --with-mpc="${PREFIX_LINUX}"       \
+    --disable-multilib                 \
+    --disable-shared                   \
+    --disable-nls                      \
+    --enable-languages=c,c++           \
+    --with-cpu=cortex-a7               \
+    --with-fpu=neon-vfpv4              \
+    --with-float=hard                  \
+    --with-newlib                      \
+    --with-headers="${SOURCE_DIR}/newlib-4.3.0.20230120/newlib/libc/include"
 fi
 make -j`nproc` && make install
 
@@ -201,92 +206,92 @@ export CC_FOR_BUILD="x86_64-linux-gnu-gcc"
 
 ################# Ready for buiding dependencies #################
 
-cd "${BUILD_DIR}/libiconv-1.17-build-windows"
+cd "${BUILD_DIR}/windows/libiconv-1.17-build"
 if [ ! -f "Makefile" ]; then
-    ../libiconv-1.17/configure    \
-    --host="${HOST_CC}"           \
-    --build="${BUILD}"            \
-    --enable-static               \
-    --disable-rpath               \
-    --prefix "${PREFIX_WIN}"      \
-    CFLAGS="-I${PREFIX_WIN}/include --std=gnu89" \
-    LDFLAGS="-L${PREFIX_WIN}/lib" \
-    CXXFLAGS="-I${PREFIX_WIN}/include"
+    ${SOURCE_DIR}/libiconv-1.17/configure \
+    --host="${HOST_CC}"                   \
+    --build="${BUILD}"                    \
+    --enable-static                       \
+    --disable-rpath                       \
+    --prefix "${PREFIX_TARGET}"           \
+    CFLAGS="-I${PREFIX_TARGET}/include --std=gnu89" \
+    LDFLAGS="-L${PREFIX_TARGET}/lib"      \
+    CXXFLAGS="-I${PREFIX_TARGET}/include"
 fi
 make -j`nproc` && make install
 
-cd "${BUILD_DIR}/gettext-0.20.2-build-windows"
+cd "${BUILD_DIR}/windows/gettext-0.20.2-build"
 if [ ! -f "Makefile" ]; then
-    ../gettext-0.20.2/configure      \
-    --host="${HOST_CC}"              \
-    --build="${BUILD}"               \
-    --disable-threads                \
-    --enable-static                  \
-    --disable-rpath                  \
-    --prefix "${PREFIX_WIN}"         \
-    CFLAGS="-I${PREFIX_WIN}/include" \
-    LDFLAGS="-L${PREFIX_WIN}/lib"    \
-    CXXFLAGS="-I${PREFIX_WIN}/include"
+    ${SOURCE_DIR}/gettext-0.20.2/configure \
+    --host="${HOST_CC}"                    \
+    --build="${BUILD}"                     \
+    --disable-threads                      \
+    --enable-static                        \
+    --disable-rpath                        \
+    --prefix "${PREFIX_TARGET}"            \
+    CFLAGS="-I${PREFIX_TARGET}/include"    \
+    LDFLAGS="-L${PREFIX_TARGET}/lib"       \
+    CXXFLAGS="-I${PREFIX_TARGET}/include"
 fi
 make -j`nproc` && make install
 
-cd "${BUILD_DIR}/gmp-6.1.0-build-windows"
+cd "${BUILD_DIR}/windows/gmp-6.1.0-build"
 if [ ! -f "Makefile" ]; then
-    ../gmp-6.1.0/configure   \
-    --prefix="${PREFIX_WIN}" \
-    --host="${HOST_CC}"      \
-    --build="${BUILD}"       \
-    --enable-fft             \
-    --enable-cxx             \
-    --disable-shared         \
+    ${SOURCE_DIR}/gmp-6.1.0/configure \
+    --prefix="${PREFIX_TARGET}"       \
+    --host="${HOST_CC}"               \
+    --build="${BUILD}"                \
+    --enable-fft                      \
+    --enable-cxx                      \
+    --disable-shared                  \
     --enable-static
 fi
 make -j`nproc` && make install
 
-cd "${BUILD_DIR}/mpfr-4.2.0-build-windows"
+cd "${BUILD_DIR}/windows/mpfr-4.2.0-build"
 if [ ! -f "Makefile" ]; then
-    ../mpfr-4.2.0/configure  \
-    --prefix="${PREFIX_WIN}" \
-    --host="${HOST_CC}"      \
-    --build="${BUILD}"       \
-    --enable-static          \
-    --disable-shared         \
-    --with-gmp="${PREFIX_WIN}"
+    ${SOURCE_DIR}/mpfr-4.2.0/configure \
+    --prefix="${PREFIX_TARGET}"        \
+    --host="${HOST_CC}"                \
+    --build="${BUILD}"                 \
+    --enable-static                    \
+    --disable-shared                   \
+    --with-gmp="${PREFIX_TARGET}"
 fi
 make -j`nproc` && make install
 
-cd "${BUILD_DIR}/mpc-1.3.1-build-windows"
+cd "${BUILD_DIR}/windows/mpc-1.3.1-build"
 if [ ! -f "Makefile" ]; then
-    ../mpc-1.3.1/configure     \
-    --prefix="${PREFIX_WIN}"   \
-    --host="${HOST_CC}"        \
-    --build="${BUILD}"         \
-    --enable-static            \
-    --disable-shared           \
-    --with-gmp="${PREFIX_WIN}" \
-    --with-mpfr="${PREFIX_WIN}"
-fi
-make -j`nproc` && make install
-
-cd "${BUILD_DIR}/isl-0.26-build-windows"
-if [ ! -f "Makefile" ]; then
-    ../isl-0.26/configure             \
-    --prefix="${PREFIX_WIN}"          \
+    ${SOURCE_DIR}/mpc-1.3.1/configure \
+    --prefix="${PREFIX_TARGET}"       \
     --host="${HOST_CC}"               \
     --build="${BUILD}"                \
-    --disable-shared                  \
     --enable-static                   \
-    --with-gmp-prefix="${PREFIX_WIN}" \
-    --prefix="${PREFIX_WIN}"
+    --disable-shared                  \
+    --with-gmp="${PREFIX_TARGET}"     \
+    --with-mpfr="${PREFIX_TARGET}"
 fi
 make -j`nproc` && make install
 
-cd "${BUILD_DIR}/expat-2.5.0-build-windows"
+cd "${BUILD_DIR}/windows/isl-0.26-build"
 if [ ! -f "Makefile" ]; then
-    ../expat-2.5.0/configure \
-    --prefix="${PREFIX_WIN}" \
-    --host="${HOST_CC}"      \
-    --build="${BUILD}"       \
+    ${SOURCE_DIR}/isl-0.26/configure     \
+    --prefix="${PREFIX_TARGET}"          \
+    --host="${HOST_CC}"                  \
+    --build="${BUILD}"                   \
+    --disable-shared                     \
+    --enable-static                      \
+    --with-gmp-prefix="${PREFIX_TARGET}" \
+    --prefix="${PREFIX_TARGET}"
+fi
+make -j`nproc` && make install
+
+cd "${BUILD_DIR}/windows/expat-2.5.0-build"
+if [ ! -f "Makefile" ]; then
+    ${SOURCE_DIR}/expat-2.5.0/configure \
+    --prefix="${PREFIX_TARGET}"         \
+    --host="${HOST_CC}"                 \
+    --build="${BUILD}"                  \
     --disable-shared
 fi
 make -j`nproc` && make install
@@ -294,101 +299,105 @@ make -j`nproc` && make install
 ############################################################################
 # Build arm-none-eabi-gcc on Windows Mingw-64
 
-cd "${BUILD_DIR}/binutils-2.40-build-windows"
+cd "${BUILD_DIR}/windows/binutils-2.40-build"
 if [ ! -f "Makefile" ]; then
-    ../binutils-2.40/configure  \
-    --host="${HOST_CC}"         \
-    --build="${BUILD}"          \
-    --target="${TARGET}"        \
-    --prefix="${PREFIX_TARGET}" \
-    --with-gmp="${PREFIX_WIN}"  \
-    --with-mpfr="${PREFIX_WIN}" \
-    --with-mpc="${PREFIX_WIN}"  \
-    CFLAGS="-I${PREFIX_WIN}/include" \
-    LDFLAGS="-L${PREFIX_WIN}/lib"    \
-    CXXFLAGS="-I${PREFIX_WIN}/include"
+    ${SOURCE_DIR}/binutils-2.40/configure \
+    --host="${HOST_CC}"                   \
+    --build="${BUILD}"                    \
+    --target="${TARGET}"                  \
+    --prefix="${PREFIX_TARGET}"           \
+    --with-gmp="${PREFIX_TARGET}"         \
+    --with-mpfr="${PREFIX_TARGET}"        \
+    --with-mpc="${PREFIX_TARGET}"         \
+    CFLAGS="-I${PREFIX_TARGET}/include"   \
+    LDFLAGS="-L${PREFIX_TARGET}/lib"      \
+    CXXFLAGS="-I${PREFIX_TARGET}/include"
 fi
 make -j`nproc` && make install-strip
 
-rm -rf "${BUILD_DIR}/gcc-13.1.0-build-windows/*"
-cd "${BUILD_DIR}/gcc-13.1.0-build-windows"
-../gcc-13.1.0/configure     \
---prefix="${PREFIX_TARGET}" \
---build="${BUILD}"          \
---host="${HOST_CC}"         \
---target="${TARGET}"        \
---with-gmp="${PREFIX_WIN}"  \
---with-mpfr="${PREFIX_WIN}" \
---with-mpc="${PREFIX_WIN}"  \
---disable-multilib          \
---disable-shared            \
---enable-static             \
---disable-nls               \
---enable-languages=c,c++    \
---with-cpu=cortex-a7        \
---with-fpu=neon-vfpv4       \
---with-float=hard           \
---with-newlib               \
---without-headers
+rm -rf ${BUILD_DIR}/windows/gcc-13.1.0-build/*
+cd "${BUILD_DIR}/windows/gcc-13.1.0-build"
+${SOURCE_DIR}/gcc-13.1.0/configure        \
+    --prefix="${PREFIX_TARGET}"           \
+    --build="${BUILD}"                    \
+    --host="${HOST_CC}"                   \
+    --target="${TARGET}"                  \
+    --with-gmp="${PREFIX_TARGET}"         \
+    --with-mpfr="${PREFIX_TARGET}"        \
+    --with-mpc="${PREFIX_TARGET}"         \
+    --disable-multilib                    \
+    --disable-shared                      \
+    --enable-static                       \
+    --disable-nls                         \
+    --enable-languages=c,c++              \
+    --with-cpu=cortex-a7                  \
+    --with-fpu=neon-vfpv4                 \
+    --with-float=hard                     \
+    --with-newlib                         \
+    --without-headers                     \
+    CFLAGS="-I${PREFIX_TARGET}/include"   \
+    CXXFLAGS="-I${PREFIX_TARGET}/include" \
+    LDFLAGS="-L${PREFIX_TARGET}/lib"
 make -j`nproc` && make install-strip
 
-cd "${BUILD_DIR}/newlib-4.3.0.20230120-build-windows"
+cd "${BUILD_DIR}/windows/newlib-4.3.0.20230120-build"
 if [ ! -f "Makefile" ]; then
-    ../newlib-4.3.0.20230120/configure \
-    --host="${HOST_CC}"         \
-    --build="${BUILD}"          \
-    --target="${TARGET}"        \
-    --prefix="${PREFIX_TARGET}" \
-    --disable-multilib          \
-    --disable-shared            \
-    --enable-static             \
-    --disable-nls               \
-    --with-cpu=cortex-a7        \
-    --with-fpu=neon-vfpv4       \
-    --with-float=hard           \
+    ${SOURCE_DIR}/newlib-4.3.0.20230120/configure \
+    --host="${HOST_CC}"                           \
+    --build="${BUILD}"                            \
+    --target="${TARGET}"                          \
+    --prefix="${PREFIX_TARGET}"                   \
+    --disable-multilib                            \
+    --disable-shared                              \
+    --enable-static                               \
+    --disable-nls                                 \
+    --with-cpu=cortex-a7                          \
+    --with-fpu=neon-vfpv4                         \
+    --with-float=hard                             \
     --disable-newlib-supplied-syscalls
 fi
 make -j`nproc` && make install
 
-rm -rf "${BUILD_DIR}/gcc-13.1.0-build-windows/*"
-cd "${BUILD_DIR}/gcc-13.1.0-build-windows"
-../gcc-13.1.0/configure     \
---prefix="${PREFIX_TARGET}" \
---build="${BUILD}"          \
---host="${HOST_CC}"         \
---target="${TARGET}"        \
---with-gmp="${PREFIX_WIN}"  \
---with-mpfr="${PREFIX_WIN}" \
---with-mpc="${PREFIX_WIN}"  \
---disable-multilib          \
---disable-shared            \
---enable-static             \
---disable-nls               \
---enable-languages=c,c++    \
---with-cpu=cortex-a7        \
---with-fpu=neon-vfpv4       \
---with-float=hard           \
---with-newlib
+rm -rf ${BUILD_DIR}/windows/gcc-13.1.0-build/*
+cd "${BUILD_DIR}/windows/gcc-13.1.0-build"
+    ${SOURCE_DIR}/gcc-13.1.0/configure    \
+    --prefix="${PREFIX_TARGET}"           \
+    --build="${BUILD}"                    \
+    --host="${HOST_CC}"                   \
+    --target="${TARGET}"                  \
+    --with-gmp="${PREFIX_TARGET}"         \
+    --with-mpfr="${PREFIX_TARGET}"        \
+    --with-mpc="${PREFIX_TARGET}"         \
+    --disable-multilib                    \
+    --disable-shared                      \
+    --enable-static                       \
+    --disable-nls                         \
+    --enable-languages=c,c++              \
+    --with-cpu=cortex-a7                  \
+    --with-fpu=neon-vfpv4                 \
+    --with-float=hard                     \
+    --with-newlib                         \
+    CFLAGS="-I${PREFIX_TARGET}/include"   \
+    CXXFLAGS="-I${PREFIX_TARGET}/include" \
+    LDFLAGS="-L${PREFIX_TARGET}/lib"
 make -j`nproc` && make install-strip
 
-cd "${BUILD_DIR}/gdb-13.1-build-windows"
+cd "${BUILD_DIR}/windows/gdb-13.1-build"
 if [ ! -f "Makefile" ]; then
-    ../gdb-13.1/configure       \
-    --host="${HOST_CC}"         \
-    --build="${BUILD}"          \
-    --target="${TARGET}"        \
-    --prefix="${PREFIX_TARGET}" \
-    --enable-64-bit-bfd         \
-    --disable-werror            \
-    --disable-win32-registry    \
-    --disable-rpath             \
+    ${SOURCE_DIR}/gdb-13.1/configure \
+    --host="${HOST_CC}"              \
+    --build="${BUILD}"               \
+    --target="${TARGET}"             \
+    --prefix="${PREFIX_TARGET}"      \
+    --enable-64-bit-bfd              \
+    --disable-werror                 \
+    --disable-nls                    \
+    --disable-win32-registry         \
+    --disable-rpath                  \
     --with-system-gdbinit="${PREFIX_TARGET}/etc/gdbinit" \
-    --with-gmp="${PREFIX_WIN}"  \
-    --with-mpfr="${PREFIX_WIN}" \
-    --with-mpc="${PREFIX_WIN}"  \
-    --with-expat="${PREFIX_WIN}" \
-    CFLAGS="-I${PREFIX_WIN}/include" \
-    LDFLAGS="-L${PREFIX_WIN}/lib -liconv -lisl -lgettextlib" \
-    CXXFLAGS="-I${PREFIX_WIN}/include"
+    --with-gmp="${PREFIX_TARGET}"    \
+    --with-mpfr="${PREFIX_TARGET}"   \
+    --with-mpc="${PREFIX_TARGET}"    \
+    --with-expat="${PREFIX_TARGET}"
 fi
 make -j`nproc` && make install-strip
